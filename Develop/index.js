5f92437a7c17dbd054939df923f9c5fc16d05104
Zachary Jayes <zacjayes@gmail.com>
2021-01-18 19:47:42 -0600

    made questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,71 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer');
 
-];
+//questions for user
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            meessag: 'What is the time',
+            name: 'title',
+        },
+        {
+            type: 'checkbox',
+            message: 'What would you like in your README?',
+            name: 'ReadMe-Options',
+            choices: ['description', 'installation', 'usage', 'contributing', 'tests'],
+        },
+        {
+            type: 'input',
+            message: 'What is your project about?',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'Describe your installation process.',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'Describe the use of your project.',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'Who should be credited for this project',
+            name: 'contributing',
+        },
+        {
+            type: 'input',
+            message: '',
+            name: 'tests',
+        },
+        {
+            type: 'checkbox' ,
+            message: 'What Licenses did you use?',
+            name: 'licenses',
+            choices: ['BSD Licesnse', 'MIT License', 'GPL License', 'Apache license', 'GNU Public Licesnse', 'Other', 'None'],
+          },
+    ])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // function to write README file
 function writeToFile(fileName, data) {
